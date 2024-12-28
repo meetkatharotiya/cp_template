e25c1c8d0c829c766391125ea68173dc8afe0ccc
Meet Katharotiya <112794965+meetkatharotiya@users.noreply.github.com>
2024-12-28 16:35:57 +0530

    Update meet_template.cpp

--- meet_template.cpp
+++ meet_template.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#pragma GCC target("popcnt")
 using namespace std;
 using namespace chrono;
 using namespace __gnu_pbds;
+
 #ifndef ONLINE_JUDGE
 #include "debug.hpp"
 #else
 #define debug(...) 8
 #endif
-#define ll long long int
+
+typedef long long int ll;
+typedef long double lld;
 #define pb push_back
-#define fast_io                       \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
-#define eb emplace_back
-#define INF 1e18
+
 #define fi(l, r) for (ll i = l; i < r; i++)
 #define fj(l, r) for (ll j = l; j < r; j++)
 #define fk(l, r) for (ll k = l; k < r; k++)
@@ -26,56 +25,49 @@ using namespace __gnu_pbds;
 #define no cout << "NO" << endl
 #define ff first
 #define ss second
-#define endl '\n'
 #define all(x) x.begin(), x.end()
 #define allr(x) x.rbegin(), x.rend()
-ll lcm(ll a, ll b) { return ((a * b) / __gcd(a, b)); }
+
 template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
 
 const ll mod = 1e9 + 7;
 const ll mod2 = 998244353;
-const ll inf = 1e9;
-const ll N = 1e6 + 3;
-
-// vector<ll> arr(N, 1);
-// void set_sieve(){arr[0]=0;arr[1]=0;for(ll i=2;i*i <N;i++){if(arr[i]==1){for(ll j=i*2;j<N;j+=i){arr[j]=0;}}}}
-// void set_fact(){arr[0]=1;for(ll i=1;i<N;i++){arr[i]=(i*arr[i-1])%mod;}}
-// vector<ll>prime;void prime_vec(){fi(0,N+1){if(arr[i]==1){prime.pb(i);}}}
+const ll inf = 1e18;
+const ll N = 2e6 + 5;
+const lld PI = 3.14159265358979323846;
 
 /*------------------------------------------------------------------------------------------------------------------------------------------*/
 
-void solve()
+void init()
 {
-    ll n;
-    cin >> n;
-    vector<ll> v(n + 1, 0);
-    fi(1, n + 1) cin >> v[i];
-    sgtree sg;
-    sg.init(n + 1);
-    sg.build(v);
-    cout << sg.calc(2, 2).g << endl;
 }
 
-int main()
+void solve()
 {
-    fast_io;
+}
+
+signed main()
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("Error.txt", "w", stderr);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
 #endif
 
-    ll t = 1;
-    // cin >> t;
-    // op_seive();
+	init();
+	ll t = 1;
+	cin >> t;
 
-    for (ll tc = 1; tc <= t; tc++)
-    {
+	for (ll tc = 1; tc <= t; tc++)
+	{
 
-        debug(tc);
-
-        solve();
-    }
-    return 0;
+		debug(tc);
+		solve();
+	}
+	return 0;
 }
